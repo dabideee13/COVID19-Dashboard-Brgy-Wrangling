e8843ee12e89857ebcfaea029f8cedc70885a22f
Eden Rose Risma <edenroserisma13@gmail.com>
2021-07-25 08:19:06 +0800

    changes made at exe.py

--- exe.py
+++ exe.py
@@ -1,8 +1,8 @@
 import subprocess
 import os
 
-# executes main.py in the terminal
-cmd1 = subprocess.Popen(["start", "cmd", "/k",
-                      "py C:/Users/Admin/Documents/GitHub/COVID19-Dashboard-Brgy-Wrangling/main.py"],
-                      shell = True)
+while 'dist' in os.getcwd():
+    os.chdir('..')
 
+# executes main.py in the terminal
+cmd = subprocess.Popen(["start", "cmd", "/k", f"py {os.getcwd()}\main.py"], shell=True)
