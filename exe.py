b2eb3220891f9705c64e330d474ac06b267ae169
Dave Emmanuel Magno <56486609+dabideee13@users.noreply.github.com>
2021-07-29 12:44:16 +0800

    Delete exe.py

--- exe.py
+++
@@ -1,8 +0,0 @@
-import subprocess
-import os
-
-while 'dist' in os.getcwd():
-    os.chdir('..')
-
-# executes main.py in the terminal
-cmd = subprocess.Popen(["start", "cmd", "/k", f"python {os.getcwd()}\main.py"], shell=True)
